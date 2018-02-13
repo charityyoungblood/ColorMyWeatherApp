06efbf56f91bb60b58e74a30338b9447415bbb21
Charity <Youngblood.charity@gmail.com>
2018-02-13 14:53:23 -0800

    created LocationWeather Model

---
+++ ColorMyWeatherApp/Model/LocationWeatherData.swift
@@ -0,0 +1,11 @@
+//
+//  LocationWeatherData.swift
+//  ColorMyWeatherApp
+//
+//  Created by Charity Youngblood on 2/13/18.
+//  Copyright © 2018 Charity Youngblood. All rights reserved.
+//
+
+import Foundation
+
+// **REMEMBER -- this is a Model, DO NOT import UIKit - the Model is UI independent ** 
