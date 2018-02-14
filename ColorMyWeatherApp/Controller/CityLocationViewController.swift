6d97688ff4d43b2eef153448ddedc5cdca8699da
Charity <Youngblood.charity@gmail.com>
2018-02-13 16:44:25 -0800

    added custom colors to CityLocationViewController

--- ColorMyWeatherApp/Controller/CityLocationViewController.swift
+++ ColorMyWeatherApp/Controller/CityLocationViewController.swift
@@ -16,19 +16,22 @@ import UIKit
 
 class CityLocationViewController: UIViewController {
 
-    var pantoneColors: UIColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
+    var pantoneColors: UIColor = UIColor(init(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0))
+    
     
     @IBOutlet weak var cityLocationBackgroundColor: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: Change background color to animate/change to a different Pantone color every few seconds
+        changeBackgroundColor()
         
     }
 
-    func changeBackgroundColor() {
-        
+    func changeBackgroundColor(){
+        cityLocationBackgroundColor.backgroundColor = pantoneColors(red: 0.0, green: 172.0, blue: 107.0, aplha: 0.0))
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
