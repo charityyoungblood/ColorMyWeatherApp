569d7f4411b5dd30ee412a8a65ba1694d818d29b
charityyoungblood <youngblood.charity@gmail.com>
2018-02-28 18:05:36 -0800

    added placeholders for color palette to main storyboard

--- ColorMyWeatherApp/Controller/WeatherColorViewController.swift
+++ ColorMyWeatherApp/Controller/WeatherColorViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class WeatherColorViewController: UIViewController {
     
-    // TODO: Display "playing card" image/view of color palette, below weather forecast
-    
+   
     // TODO: Display weather condition image
     
-    // TODO: Display temperature °F/°C 
-
+    // TODO: Display temperature °F/°C
+    
+    // TODO: Display "playing card" image/view of color palette, below weather forecast
+    
+// what items have background color?
+    // you could change the background color property of a label or view and display like a palette 
     override func viewDidLoad() {
         super.viewDidLoad()
 
