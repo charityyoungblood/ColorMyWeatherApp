fe11a8b8296157eb1ee3bf6702380b19fee94d3a
charityyoungblood <youngblood.charity@gmail.com>
2018-02-28 18:20:44 -0800

    added for loop for color palette; added color placeholders to WeatherColorViewController

--- ColorMyWeatherApp/Controller/WeatherColorViewController.swift
+++ ColorMyWeatherApp/Controller/WeatherColorViewController.swift
@@ -19,7 +19,26 @@ class WeatherColorViewController: UIViewController {
     
     @IBOutlet var colorPalette: [UILabel]!
     
-    
+    func changeColorPalette() {
+        // this function will change the color of each label in the colorPalette array
+        for color in colorPalette {
+            if color.tag == 1 {
+                color.backgroundColor = #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1)
+            }
+            
+            if color.tag == 2 {
+                color.backgroundColor = #colorLiteral(red: 0.09411764706, green: 0.1568627451, blue: 0.2862745098, alpha: 1)
+            }
+            
+            if color.tag == 3 {
+                color.backgroundColor = #colorLiteral(red: 0.1254901961, green: 0.3450980392, blue: 0.3764705882, alpha: 1)
+            }
+            
+            if color.tag == 4 {
+                color.backgroundColor = #colorLiteral(red: 0.8941176471, green: 0.8911558799, blue: 0.7292792632, alpha: 1)
+            }
+        }
+    }
 // what items have background color?
     // you could change the background color property of a label or view and display like a palette 
     override func viewDidLoad() {
